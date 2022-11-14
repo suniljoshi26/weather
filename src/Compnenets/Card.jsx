a78a7ea098b14d8545b077d155129fdbd9caa314
Sunil Joshi <joshisunil2601@gmail.com>
2022-11-14 21:45:06 +0530

    api intergation

--- src/Compnenets/Card.jsx
+++ src/Compnenets/Card.jsx
@@ -1,70 +1,70 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+// import axios from "axios";
+// import React, { useEffect, useState } from "react";
 
-import Weather from "./Weather";
+// import Weather from "./Weather";
 
-const Card = () => {
-  const [search, setSearch] = useState("london");
-  const [data, setData] = useState([]);
-  const [input, setInput] = useState("");
-  console.log("vgghsdhsdh");
-  useEffect(() => {
-    console.log("vgghsdhsdh");
-    axios
-      .get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=061602ffb426fc9a2f8cb137ac8b452c`
-      )
-      .then((res) => {
-        setData(res.data);
-        console.log("ad", res.data);
-      });
-  }, [search]);
+// const Card = () => {
+//   const [search, setSearch] = useState("london");
+//   const [data, setData] = useState([]);
+//   const [input, setInput] = useState("");
+//   console.log("vgghsdhsdh");
+//   useEffect(() => {
+//     console.log("vgghsdhsdh");
+//     axios
+//       .get(
+//         `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=061602ffb426fc9a2f8cb137ac8b452c`
+//       )
+//       .then((res) => {
+//         setData(res.data);
+//         console.log("ad", res.data);
+//       });
+//   }, [search]);
 
-  const handlesubmit = (event) => {
-    console.log("dsadasds");
-    event.preventDefault();
-    setSearch(input);
-    console.log("dsadasds");
-  };
+//   const handlesubmit = (event) => {
+//     console.log("dsadasds");
+//     event.preventDefault();
+//     setSearch(input);
+//     console.log("dsadasds");
+//   };
 
-  return (
-    <div>
-      <div className="container mt-5 ">
-        <div className="row justify-content-center">
-          <div className="col-md-4 ">
-            <div className="card text-bg-dark text-center border-0 ">
-              <img
-                src="https://images.unsplash.com/photo-1504608524841-42fe6f032b4b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1965&q=80"
-                className="card-img"
-                alt="..."
-              />
-              <div className="card-img-overlay">
-                <form onSubmit={handlesubmit}>
-                  <div className="input-group mb-3">
-                    <input
-                      type="text"
-                      className="form-control"
-                      value={input}
-                      onChange={(e) => setInput(e.target.value)}
-                    />
-                    <div className="input-group-append">
-                      <button
-                        className="btn btn-outline-secondary"
-                        type="submit"
-                      >
-                        <i className="fa fa-search "></i>
-                      </button>
-                    </div>
-                  </div>
-                </form>
-                <Weather data={data} />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <div className="container mt-5 ">
+//         <div className="row justify-content-center">
+//           <div className="col-md-4 ">
+//             <div className="card text-bg-dark text-center border-0 ">
+//               <img
+//                 src="https://images.unsplash.com/photo-1504608524841-42fe6f032b4b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1965&q=80"
+//                 className="card-img"
+//                 alt="..."
+//               />
+//               <div className="card-img-overlay">
+//                 <form onSubmit={handlesubmit}>
+//                   <div className="input-group mb-3">
+//                     <input
+//                       type="text"
+//                       className="form-control"
+//                       value={input}
+//                       onChange={(e) => setInput(e.target.value)}
+//                     />
+//                     <div className="input-group-append">
+//                       <button
+//                         className="btn btn-outline-secondary"
+//                         type="submit"
+//                       >
+//                         <i className="fa fa-search "></i>
+//                       </button>
+//                     </div>
+//                   </div>
+//                 </form>
+//                 <Weather data={data} />
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Card;
+// // export default Card;
--- src/Compnenets/weather.jsx
+++ src/Compnenets/weather.jsx
@@ -1,29 +1,32 @@
 import React from "react";
 
 const Weather = ({ data }) => {
-  console.log("data is a", data);
-  let emoji = null;
-  if (data.main != "undefined") {
-    if (data.weather[0].main == Clouds) {
-      emoji = "fa-cloud";
-    }
-  }
+  // const { name, main } = data;
+  // const { temp_min, temp_max, temp } = main;
 
-  let temp = (data.main.temp - 273.15).toFixed(2);
-  let temp_min = (data.main.temp_min - 273.15).toFixed(2);
-  let temp_max = (data.main.temp_max - 273.15).toFixed(2);
+  console.log("gfgh", data.main.temp);
+  // let emoji = null;
+  // if (data.main != "undefined") {
+  //   if (data.weather[0].main == Clouds) {
+  //     emoji = "fa-cloud";
+  //   }
+  // }
+
+  // let temp = (data.main.temp - 273.15).toFixed(2);
+  // let temp_min = (data.main.temp_min - 273.15).toFixed(2);
+  // let temp_max = (data.main.temp_max - 273.15).toFixed(2);
   return (
     <div>
       {" "}
       <div className=" bg-dark bg-opacity-50 py-3 justify-content-center mt-4">
-        <h2 className="card-title">{data.name}</h2>
+        <h2 className="card-title"></h2>
         <p className="card-text">thursday, october 14,2022</p>
         <hr />
         <i className="fas ${emoji} fa-4x"></i>
-        <h1 className=" fx-border mb-5">{temp}&deg;C</h1>
-        <p className=" fw-bolder mb-0 lead">{data.weather[0].main}</p>
+        <h1 className=" fx-border mb-5"> {data.main.temp} </h1>
+        <p className=" fw-bolder mb-0 lead">{/* {data.weather[0].main} */}</p>
         <p className="lead mt-2 ">
-          {temp_min}&deg;C || {temp_max}&deg;C{" "}
+          {/* {temp_min}&deg;C || {temp_max}&deg;C{" "} */}
         </p>
       </div>
     </div>
